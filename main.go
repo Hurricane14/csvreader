c48598240ac5753bcd772b49adf85bec4ccd2ec1
Hurricane14 <79533040187@yandex.ru>
2023-03-04 16:38:36 +0300

    Remove log package from main()

--- main.go
+++ main.go
@@ -3,34 +3,32 @@ package main
 import (
 	"csvreader/teval"
 	"flag"
-	"log"
+	"fmt"
 	"os"
 	"runtime"
 	"runtime/pprof"
 )
 
 func main() {
-	log.SetFlags(0)
-
 	sep := flag.String("s", ",", "change separator")
 	shouldEvaluate := flag.Bool("e", false, "evaluate table")
 	cpuProfile := flag.String("cpuprofile", "", "write cpu profile to file")
 	memProfile := flag.String("memprofile", "", "write memory profile to file")
 	flag.Parse()
 	if flag.NArg() < 1 {
-		log.Fatal("usage: csvreader *filename*")
+		exit(fmt.Errorf("usage: csvreader *filename*"))
 	}
 
 	// Start profiling
 	if *cpuProfile != "" {
 		file, err := os.Create(*cpuProfile)
 		if err != nil {
-			log.Fatal(err)
+			exit(err)
 		}
 		defer file.Close()
 
 		if err := pprof.StartCPUProfile(file); err != nil {
-			log.Fatal(err)
+			exit(err)
 		}
 		defer pprof.StopCPUProfile()
 	}
@@ -38,35 +36,40 @@ func main() {
 	filename := flag.Args()[0]
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Fatal(err)
+		exit(err)
 	}
 	defer file.Close()
 
 	table, err := teval.Read(file, *sep)
 	if err != nil {
-		log.Fatal(err)
+		exit(err)
 	}
 
 	if *shouldEvaluate {
 		if err := table.EvalAll(); err != nil {
-			log.Fatal(err)
+			exit(err)
 		}
 	}
 
 	if *memProfile != "" {
 		file, err := os.Create(*memProfile)
 		if err != nil {
-			log.Fatal(err)
+			exit(err)
 		}
 		defer file.Close()
 
 		runtime.GC()
 		if err := pprof.WriteHeapProfile(file); err != nil {
-			log.Fatal(err)
+			exit(err)
 		}
 	}
 
 	if err := teval.Write(os.Stdout, table, *sep); err != nil {
-		log.Fatal(err)
+		exit(err)
 	}
+}
+
+func exit(err error) {
+	fmt.Fprintln(os.Stderr, err.Error())
+	os.Exit(1)
 }
